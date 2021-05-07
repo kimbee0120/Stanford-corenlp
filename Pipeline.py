23d748d33addba91aa2aa30c55418a2b6acbb37b
Eunbi Kim <kimeunb1822@gmail.com>
2021-05-07 10:11:03 -0400

    Update Pipeline.py

--- Pipeline.py
+++ Pipeline.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-
-import stanfordnlp
-stanfordnlp.download('en')
-nlp = stanfordnlp.Pipeline()
-doc = nlp("Barack Obama was born in Hawaii.  He was elected president in 2008.")
-doc.sentences[0].print_dependencies()
-
+import stanza
+stanza.download('en') 
+nlp = stanza.Pipeline('en') # initialize English neural pipeline
+doc = nlp("Barack Obama was born in Hawaii.")
+print(doc)
+print(doc.entities)
